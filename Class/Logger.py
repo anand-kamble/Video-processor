228621752dd6f20fa8da4633ee884326026d4cc2
anand-kamble <anandmk837@gmail.com>
2023-04-05 22:46:11 +0530

    Updated logger

--- Class/Logger.py
+++ Class/Logger.py
@@ -8,12 +8,19 @@ class Logger:
 
     def log(self, msg: str) -> None:
         logString: str = '[' + self.scope + '] : ' + msg
+        self.__log("[LOG]" + msg)
         print(Fore.BLUE + logString + Style.RESET_ALL)
 
     def warn(self, msg: str) -> None:
         logString: str = '[' + self.scope + '] : ' + msg
+        self.__log("[WARN]" + msg)
         print(Fore.YELLOW + logString + Style.RESET_ALL)
 
     def error(self, msg: str) -> None:
         logString: str = '[' + self.scope + '] : ' + msg
+        self.__log("[ERROR]" + msg)
         print(Fore.RED + logString + Style.RESET_ALL)
+
+    def __log(self, msg: str):
+        # Stuff like writing to a file or posting to server goes here.
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from ffmpeg import input
+from ffmpeg import input, output, run
 from Class.CONSTANTS import VIDEO_PROCESSOR_CONSTANTS_CLASS
 
 CONSTANTS = VIDEO_PROCESSOR_CONSTANTS_CLASS()
@@ -6,3 +6,9 @@ CONSTANTS = VIDEO_PROCESSOR_CONSTANTS_CLASS()
 
 CONSTANTS.create(
     "VIDEO_PATH", "C:/Users/91911/Documents/JDownloader/Miley Cyrus - Flowers (Official Video) (1406p_24fps_AV1-128kbit_AAC).mp4")
+
+inp = input(CONSTANTS.use("VIDEO_PATH"))
+# audio = inp.audio.filter("aecho", 0.8, 0.9, 1000, 0.3)
+video = inp.video.hflip()
+# out = output(video, 'out.mp4')
+run(inp)
