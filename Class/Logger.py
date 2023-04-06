cffeca3dd7caae823a67521b35963da19f06fba3
anand-kamble <anandmk837@gmail.com>
2023-04-06 09:55:09 +0530

    Sucessfully added ffmpeg

--- Class/Logger.py
+++ Class/Logger.py
@@ -1,26 +1,32 @@
 from colorama import Fore, Style
+import datetime
 
 
 class Logger:
-    def __init__(self, scope: str) -> None:
+    def __init__(self, scope: str):
         self.scope = scope or ""
         pass
 
-    def log(self, msg: str) -> None:
+    def log(self, msg: str):
         logString: str = '[' + self.scope + '] : ' + msg
-        self.__log("[LOG]" + msg)
+        self.__log(
+            datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")+" \n\t[LOG] @" + logString)
         print(Fore.BLUE + logString + Style.RESET_ALL)
 
-    def warn(self, msg: str) -> None:
+    def warn(self, msg: str):
         logString: str = '[' + self.scope + '] : ' + msg
-        self.__log("[WARN]" + msg)
+        self.__log(
+            datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")+" \n\t[WARN] @" + logString)
         print(Fore.YELLOW + logString + Style.RESET_ALL)
 
-    def error(self, msg: str) -> None:
+    def error(self, msg: str):
         logString: str = '[' + self.scope + '] : ' + msg
-        self.__log("[ERROR]" + msg)
+        self.__log(
+            datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")+" \n\t[ERROR] @" + logString)
         print(Fore.RED + logString + Style.RESET_ALL)
 
     def __log(self, msg: str):
-        # Stuff like writing to a file or posting to server goes here.
+        file1 = open("myfile.txt", "a")
+        file1.write(msg+'\n')
+        file1.close()
         pass
